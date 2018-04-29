ae4c8c221a0ef4419278967e2ba4adcc1eafa145
ramya keerthana potla <potlar1@hawkmail.newpaltz.edu>
2018-04-29 18:16:07 -0400

    nodemon, typescript, webpack installations

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+var express = require('express')
+var app = express()
+ 
+const servername = "localhost";
+const port = 3000;
+
+const simple = require('./simpleController');
+
+
+app.use('/simple', simple).listen(port);
+
+console.log("running on http://" + servername + ":" + port) 
